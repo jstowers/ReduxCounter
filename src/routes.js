26d0d8558cfc362ddf814378b625071ec3c59820
Joe Stowers <greenchiletexan@gmail.com>
2017-05-03 23:25:06 -0500

    Add router

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// May 3, 2017
+
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import Counter from './containers/counter'
+
+export default {
+
+	<Route path = '/' component = { App } >
+		<IndexRoute component = { Counter }/>
+	</Route>
+}
